effc133bab784979da2ee56f4112ee44ecc40c2c
kalul <kalul@DENGXUAN-SURFAC>
2016-11-22 22:39:32 +0800

    修改 实现

---
+++ Assets/Editor/Sample/EditorFramework/ScrollPanelWindow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using EditorFramework;
+using EditorFramework.Controls;
+using System.Collections.Generic;
+using EditorFramework.Panel;
+using System.Linq;
+
+public class ScrollPanelWindow : EditorWindowEx {
+    [MenuItem("EditorFramework/ScrollPanelWindow")]
+    static void DoIt() {
+        GetWindow<ScrollPanelWindow>();
+    }
+
+    List<Button> buttons = new List<Button>();
+    private Vector2 scrollPos;
+    Button btn = new Button("测试");
+
+    void OnEnable() {
+
+        btn.Position = new Rect(100, 100, 100, 20);
+        for(int i = 0; i < 100; i++) {
+            buttons.Add(new Button("测试" + i) );
+        }
+
+
+    }
+
+    float height = 0f;
+    bool initialized = false;
+    Rect scrollRect;
+
+    void OnGUI() {
+        
+
+        Visual.DebugMode = GUILayout.Toggle(Visual.DebugMode, "调试模式");
+
+        scrollPos = GUILayout.BeginScrollView(scrollPos);
+
+        if (initialized)
+            GUILayout.Box("",GUIStyle.none, GUILayout.Height(height));
+
+        foreach(Button btn in buttons) {
+            if (btn.Initialized)
+                btn.Render();
+            else
+                btn.RenderLayout();
+        }
+
+        GUILayout.EndScrollView();
+
+        if (Event.current.type == EventType.repaint) {
+            initialized = true;
+            height = buttons.Sum(btn =>btn.Height+btn.Style.margin.top);
+            scrollRect = GUILayoutUtility.GetLastRect();
+            Debug.Log(scrollRect);
+        }
+
+    }
+}
